32c020c4fc69bdfef79e186db00998b5edcd3a60
MarekWej <marekwejksznia17@gmail.com>
2023-06-28 12:11:43 +0200

    changes

--- Scenario.py
+++ Scenario.py
@@ -112,17 +112,26 @@ class Scenario(Pilot, Aircraft):
             "2": "Passengers",
             "3": "Second pilot"
         }
-        current_answer = "Air traffic control tower"
+        current_answer = ["Air traffic control tower", "Passengers"]
         user_choice = input(
             "We want to ask for permission to take off, who do we need to notify?\n1. Air traffic control tower\n2. "
             "Passengers\n3. Second pilot\nSelect the option: ").strip().lower()
-        if user_choice in options:
-            if options[user_choice] == current_answer:
+
+        if user_choice == "1 2" or user_choice == "2 1":
+            time.sleep(2)
+            print("Yes sir - I am informing the air traffic control tower and passengers of my intention to take off.")
+            print("-" * 100)
+            time.sleep(2)
+            self.LaunchSequence_collisionLights()
+        elif user_choice in options:
+            if options[user_choice] in current_answer:
                 time.sleep(2)
-                print("Yes sir - I am informing the air traffic control tower of my intention to take off.")
-                print("-"*100)
+                print("Yes sir - I am informing the", options[user_choice], "of my intention to take off, But we need "
+                                                                            "to inform someone else too")
+                print("-" * 100)
                 time.sleep(2)
-                self.LaunchSequence_collisionLights()
+                print("Let's try again")
+                self.LaunchSequence_info()
             else:
                 print("Wrong answer!!!")
                 time.sleep(2)
@@ -143,24 +152,76 @@ class Scenario(Pilot, Aircraft):
 
         current_answer = "Turn on blinking collision lights"
         user_choice = input(
-            "We need to let everyone around us know that we are starting the launch sequence. What do we need to do?\n1."
-            "Including engines\n2. Turn on blinking collision lights\n3. Hide the landing gear of the aircraft\nSelect the option: "
+            "We need to let everyone around us know that we are starting the launch sequence. What do we need to do?\n1. "
+            "Including engines\n2. Turn on blinking collision lights\n3. Hide the landing gear of the aircraft"
+            "\nSelect the option: "
         ).strip().lower()
 
         if user_choice in options:
             if options[user_choice] == current_answer:
                 time.sleep(2)
                 print("That's right, I turn on the collision lights. They appear to be blinking. ")
+                print('-'*100)
+                time.sleep(2)
+                self.LaunchSequence_engines()
             else:
                 print("Wrong answer!!!")
                 time.sleep(2)
                 print("Let's try again")
+                print('*'*100)
+                time.sleep(2)
                 self.LaunchSequence_collisionLights()
         else:
             print("Incorrect selection option!!!")
             time.sleep(2)
             print("Let's try again")
+            print('*' * 100)
+            time.sleep(2)
             self.LaunchSequence_collisionLights()
+
+
+    def LaunchSequence_engines(self):
+        options = {
+            "1": "Refuel the aircraft",
+            "2": "Open the luggage compartment",
+            "3": "Turn on the engines one at a time"
+        }
+
+        current_answer = "Turn on the engines one at a time"
+        user_choice = input(
+            "We need to generate the thrust needed to get the plane up. What do we need to do?\n1. "
+            "Refuel the aircraft\n2. Open the luggage compartment\n3. Turn on the engines one at a time\n"
+            "Select the option: ").strip().lower()
+
+        if user_choice in options:
+            if options[user_choice] == current_answer:
+                time.sleep(2)
+                print("Of course, I'm starting to run engine #1")
+                time.sleep(4)
+                print("Engine #1 has warmed up, I am starting to start engine #2")
+                print("-"*100)
+                time.sleep(4)
+                print("Pilot, we are now taxiing...")
+                time.sleep(4)
+                print("We are gaining launch speed...")
+
+            else:
+                print("Wrong answer!!!")
+                time.sleep(2)
+                print("Let's try again")
+                print('*' * 100)
+                time.sleep(2)
+                self.LaunchSequence_engines()
+        else:
+            print("Incorrect selection option!!!")
+            time.sleep(2)
+            print("Let's try again")
+            print('*' * 100)
+            time.sleep(2)
+            self.LaunchSequence_engines()
+
+
+
 
 
 
